c4e9c29dcbb18dde8aaa643c93568e27065add78
stLmpp <gui.stlmpp@hotmail.com>
2022-05-17 23:28:02 -0300

    - added time interceptor

---
+++ src/5-infrastructure/security/time.interceptor.ts
@@ -0,0 +1,10 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { delayWhen, interval, Observable } from 'rxjs';
+import { random } from 'st-utils';
+
+@Injectable()
+export class TimeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(delayWhen(() => interval(random(25, 75))));
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { ControllerModule } from '@controller/controller.module';
 import { MikroOrmOptions } from '@infrastructure/database/mikro-orm-options';
 import { Environment } from '@infrastructure/environment/environment';
 import { HandleErrorFilter } from '@infrastructure/error/handle-error.filter';
+import { TimeInterceptor } from '@infrastructure/security/time.interceptor';
 
 @Module({
   imports: [MikroOrmModule.forRootAsync({ useClass: MikroOrmOptions }), ControllerModule],
   providers: [
     { provide: Environment, useValue: Environment.getInstance() },
     { provide: APP_FILTER, useClass: HandleErrorFilter },
+    { provide: APP_INTERCEPTOR, useClass: TimeInterceptor },
   ],
 })
 export class AppModule {}
